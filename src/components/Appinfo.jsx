bab0d1db78b06a863e7a8c8b83b09aa5ad730300
khadijaZafar2511 <khadijazafar2511@gmail.com>
2026-01-17 20:55:31 +0500

    setting UI

--- src/components/Appinfo.jsx
+++ src/components/Appinfo.jsx
@@ -4,8 +4,8 @@ import PropTypes from 'prop-types'
 export default function Appinfo(props) {
     return (
       <>
-        <div className="w-1/2 md:h-[80vh] h-1/2 ml-15 mb-20 text-green-500 flex flex-col items-center justify-center">
-          <h1 className="font-medium text-6xl flex ml-10 ">
+        <div className="w-1/2 md:h-[80vh] h-1/2 ml-15 mt-4 lg:mt-0 mb-20 text-green-500 flex flex-col items-center justify-center">
+          <h1 className="font-medium text-6xl flex lg:ml-10  mr-19">
             Weather<p className="text-white ">App</p>
           </h1>
           <br />
@@ -13,7 +13,7 @@ export default function Appinfo(props) {
             <p className="text-white ">using</p>react.js
           </h1>
           <img
-            className="h-[20vh] w-[30vh]  ml-25 mt-2"
+            className="h-[20vh] w-[30vh]  ml-25 mt-3"
             src="\cloud2.png"
           ></img>
         </div>
